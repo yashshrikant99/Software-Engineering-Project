ca90c3a6b362774a1555ae934f07a36c4f6aa5d4
Sahil Bhola <sahilbhola@sahils-mbp.mynetworksettings.com>
2023-10-28 08:07:46 -0400

    created express JS 3 tier architecture folder structure to follow

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 var request = require('request')
-
+require('dotenv').config()
 const app = express()
 
-const port = 8080
+const port = process.env.PORT 
 app.listen(port, () => console.log(`Server listening at port ${port}`))
 
-app.get('/', (req, res) => {
+
+
+app.get('/', (req, res, next) => {
   res.send('Welcome to Stock Trainer backend')
 })
 
-app.get('/stocks', (req, res) => {
-  res.send('About route')
-})
+
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+
+const port = 8080
+app.listen(port, () => console.log(`Server listening at port ${port}`))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Stock Trainer backend')
+})
+
+app.get('/stocks', (req, res) => {
+  res.send('About route')
+})
