6a8937c4d4252c14ecd00d5567ddeaa93cb212c7
siddhkasera <smk339@scarletmail.rutgers.edu>
2023-11-05 02:22:29 -0500

    updated registration form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,11 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-// import { faCopyright} from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
   return (
-    <div>
-      <footer className="bg-dark text-center text-white">
+
+      <footer className="bg-dark text-center text-white footer-class">
         <div class="logo-text" style={{fontSize:30}}>
           TradeTrackr
         </div>
@@ -18,14 +17,13 @@ function Footer() {
         <div class="text-center p-3" style={{color: "white"}}>
            © 2023 Copyright Trade Trackr
         </div>
-        <section class ="icons">
+        <div class ="icons">
           <FontAwesomeIcon icon={faFacebook} fontSize={30}/>
           <FontAwesomeIcon icon={faXTwitter} fontSize={30}/>
           <FontAwesomeIcon icon={faInstagram} fontSize={30}/>
           <FontAwesomeIcon icon={faLinkedin} fontSize={30}/>
-        </section>
+        </div>
       </footer>
-    </div>
   )
 }
 
