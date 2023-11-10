4b7eb3c77f02393051ec5e6e84ec3d2eed854cab
Sahil Bhola <sahilbhola@sahils-mbp.mynetworksettings.com>
2023-11-09 21:08:02 -0500

    created api for login and send email and put check on creation

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -1,6 +1,7 @@
 const {Op, sequelize, DataTypes} = require('sequelize');
 const db = require("../models")
 const User = db.user
+const bcrypt = require("bcryptjs");
 
 
 async function Create(data){
@@ -12,6 +13,43 @@ async function Create(data){
     }
 }
 
+async function findEmail(email){
+    try{
+        const user = await User.findOne({where:{email}})
+        console.log(user)
+        if(!user)
+        console.log("Email not found",user)
+        return user;
+    }catch(err){
+        console.log("Error finding email",err.message)
+    }
+}
+
+async function findUserCredential(email, password){
+    try{
+        let isMatch = false
+        const user = await findEmail(email);
+        if(user){
+             isMatch = await bcrypt.compare(password,user.password);
+            return isMatch
+        }
+        return isMatch;
+    }catch(err){
+        console.log("Error checking credentials",err.message);
+    }
+}
+
+async function checkUserExist(username, email){
+    try{
+        const exist = await User.findOne({where:{username, email}})
+        return exist
+    }catch(err){
+        console.log("Cant Find User Existence", err.message)
+    }
+}
+
 module.exports = {
     Create,
+    findUserCredential,
+    checkUserExist,
 }
