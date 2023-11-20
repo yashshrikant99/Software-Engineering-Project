a15c6ac35f5277cdc8acb2e1dfb19afe89feb922
siddhkasera <smk339@scarletmail.rutgers.edu>
2023-11-19 19:48:15 -0500

    search implementation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -18,17 +18,6 @@ function Dashboard() {
     <Container className ="main-container" maxWidth= {false} sx = {{display: "flex", flexDirection: "row",height:"100%"}}>
       <Box sx = {{display:"flex", flexDirection:"column",height:"100%", width:"35%", p:"0", mr:3}}>
 
-      {/* <Box className ="typographyBox" sx={{display:"flex", flexDirection:"row", gap:"2em", pt:"5px", pl:"2px", ml:2}}>
-                <Typography sx ={{}}>
-                      <strong sx={{ml:1}}>NIFTY 50</strong>
-                      <span style={{color:"red", marginLeft:1}}>(19751.05)</span>
-                        -42.95(-0.22%)</Typography>
-                <Typography sx ={{ p:"2px"}}>
-                      <strong>SENSEX</strong>
-                      <span style={{color:"red"}}>(66282.74)</span>
-                        -42.95(-0.22%)</Typography>
-        </Box> */}
-
       <Box className="search-bar" sx ={{height:"5%", mt:4, mb:1}} >
           <SearchBar/>
       </Box>
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,45 +1,44 @@
 // import React from 'react';
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
 // import FormControlLabel from '@mui/material/FormControlLabel';
 // import Checkbox from '@mui/material/Checkbox';
-import {Link} from "react-router-dom";
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Formik, Form } from 'formik';
-import '../cssfiles/RegistrationForm.css';
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Formik, Form } from "formik";
+import "../cssfiles/RegistrationForm.css";
+import axios from "axios";
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker';
- import dayjs, { Dayjs } from 'dayjs';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import dayjs, { Dayjs } from "dayjs";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-
-const initialValues ={
+const initialValues = {
   username: "",
   email: "",
   phone: "",
-  DoB:dayjs(Date.now()),
-  password: ""
+  DoB: dayjs(Date.now()),
+  password: "",
 };
 
 const validate = (values) => {
   let errors = {};
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-  if(!values.username){
-    errors.username ="Username is required";
-  }else if (values.username.length < 6){
-      errors.username ="Username should be atleast 6 characters";
+  if (!values.username) {
+    errors.username = "Username is required";
+  } else if (values.username.length < 6) {
+    errors.username = "Username should be atleast 6 characters";
   }
   if (!values.email) {
     errors.email = "Email is required";
@@ -56,195 +55,217 @@ const validate = (values) => {
   } else if (values.DoB.length < 8) {
     errors.DoB = "Invalid DOB";
   }
-
-
   if (!values.password) {
     errors.password = "Password is required";
   } else if (values.password.length < 4) {
     errors.password = "Password too short";
   }
-
-
   return errors;
 };
-const submitForm = async(values) => {
+
+const submitForm = async (values) => {
   console.log(values);
-  axios.post("http://localhost:8081/users", values).then((response) => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.error("This is error" + err);
-  })  
+  axios
+    .post("http://localhost:8081/users", values)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.error("This is error" + err);
+    });
 };
 
 function RegistrationForm() {
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      username: data.get('username'),
-      email: data.get('email'),
-      phone: data.get('phone'),
-      DoB: data.get('DoB'),
-      password: data.get('password'),
+      username: data.get("username"),
+      email: data.get("email"),
+      phone: data.get("phone"),
+      DoB: data.get("DoB"),
+      password: data.get("password"),
     });
   };
   const defaultTheme = createTheme();
   return (
-  <Formik initialValues={initialValues}
-  validate={validate}
-  onSubmit={submitForm}
-  >
-    {(formik)=>{
-      const {
-        values,
-        handleChange,
-        handleSubmit,
-        errors,
-        touched,
-        handleBlur,
-        isValid,
-        dirty
-      } = formik;
-      return (
-        <div>
-          <ThemeProvider theme={defaultTheme}>
-          <Container component="main" maxWidth="xs" className='registration'>
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign up
-              </Typography>
-              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                  <TextField
-                      required
-                      fullWidth
-                      id="username"
-                      label="Username"
-                      name="username"
-                      autoComplete="username"
-                      value= {values.username}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      className={errors.username && touched.username?
-                      "input-error" : null}
-                    />
-                    {errors.username && touched.username && (
-                    <span className="error">{errors.username}</span>
-                    )}
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-                      value= {values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      className={errors.email && touched.email ?
-                      "input-error" : null}
-                    />
-                    {errors.email && touched.email && (
-                    <span className="error">{errors.email}</span>
-                    )}
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="phone"
-                      label="Phone Number"
-                      name="phone"
-                      autoComplete="phone"
-                      value={values.phone}
-                      onChange={handleChange}
-                      onBlur = {handleBlur}
-                      className={errors.phone && touched.phone?
-                      "input-error" : null}
-                    />
-                      {errors.phone && touched.phone && (
-                        <span className='error'>{errors.phone}</span>
-                      )}
-                  </Grid>
-                  <Grid item xs = {12}className='date-picker' >
-                  <LocalizationProvider item xs = {12}dateAdapter={AdapterDayjs} >
-                      <DemoContainer components={['DatePicker']} >
-                      <DatePicker label="DoB"
-                      required
-                      fullWidth
-                      id ="DoB"
-                      name="DoB"
-                      autoComplete="DoB"
-                      value={values.DoB}
-                      onChange={(val)=>values.DoB=val}
-                      className={errors.DoB && touched.DoB?
-                        "input-error" : null}
-                      />
-                      </DemoContainer>
-                  </LocalizationProvider>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="new-password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      className={errors.password && touched.password ?
-                       "input-error" : null}
-                    />
-                       {errors.password && touched.password && (
-                       <span className="error">{errors.password}</span>
-                  )}
-                  </Grid>
-                </Grid>
-                <Button
-                  type="submit"
-                  // className={dirty && isValid ? "" : "disabled-btn"}
-                  // disabled={!(dirty && isValid)}
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
+     <Formik
+       initialValues={initialValues}
+       validate={validate}
+       onSubmit={submitForm}
+     >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty,
+        } = formik;
+        return (
+          <div>
+            <ThemeProvider theme={defaultTheme}>
+              <Container
+                component="main"
+                maxWidth="xs"
+                className="registration"
+              >
+                <CssBaseline />
+                <Box
+                  sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
                 >
-                  Sign Up
-                </Button>
-                <Grid container justifyContent="flex-center">
-                  <Grid item className='already-acct'>
-                    <Link to="/signin" variant="body2">
-                      Already have an account? Sign in
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Box>
-          </Container>
-        </ThemeProvider>
-       </div>
-      );
-    }}
+                  <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                    <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography component="h1" variant="h5">
+                    Sign up
+                  </Typography>
+                  <Box
+                    component="form"
+                    noValidate
+                    onSubmit={handleSubmit}
+                    sx={{ mt: 3 }}
+                  >
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="username"
+                          label="Username"
+                          name="username"
+                          autoComplete="username"
+                          value={values.username}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          className={
+                            errors.username && touched.username
+                              ? "input-error"
+                              : null
+                          }
+                        />
+                        {errors.username && touched.username && (
+                          <span className="error">{errors.username}</span>
+                        )}
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          value={values.email}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          className={
+                            errors.email && touched.email ? "input-error" : null
+                          }
+                        />
+                        {errors.email && touched.email && (
+                          <span className="error">{errors.email}</span>
+                        )}
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="phone"
+                          label="Phone Number"
+                          name="phone"
+                          autoComplete="phone"
+                          value={values.phone}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          className={
+                            errors.phone && touched.phone ? "input-error" : null
+                          }
+                        />
+                        {errors.phone && touched.phone && (
+                          <span className="error">{errors.phone}</span>
+                        )}
+                      </Grid>
+                      <Grid item xs={12} className="date-picker">
+                        <LocalizationProvider
+                          item
+                          xs={12}
+                          dateAdapter={AdapterDayjs}
+                        >
+                          <DemoContainer components={["DatePicker"]}>
+                            <DatePicker
+                              label="DoB"
+                              required
+                              fullWidth
+                              id="DoB"
+                              name="DoB"
+                              autoComplete="DoB"
+                              value={values.DoB}
+                              onChange={(val) => (values.DoB = val)}
+                              className={
+                                errors.DoB && touched.DoB ? "input-error" : null
+                              }
+                            />
+                          </DemoContainer>
+                        </LocalizationProvider>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          value={values.password}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          className={
+                            errors.password && touched.password
+                              ? "input-error"
+                              : null
+                          }
+                        />
+                        {errors.password && touched.password && (
+                          <span className="error">{errors.password}</span>
+                        )}
+                      </Grid>
+                    </Grid>
+                    <Button
+                      type="submit"
+                      // className={dirty && isValid ? "" : "disabled-btn"}
+                      // disabled={!(dirty && isValid)}
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-center">
+                      <Grid item className="already-acct">
+                        <Link to="/signin" variant="body2">
+                          Already have an account? Sign in
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </Box>
+              </Container>
+            </ThemeProvider>
+          </div>
+        );
+      }}
     </Formik>
-    );
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
