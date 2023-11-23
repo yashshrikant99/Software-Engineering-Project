9d08a7ce5fadf105439f8b3ee9c95268dcfa86a0
Sahil Bhola <sahilbhola@sahils-mbp.mynetworksettings.com>
2023-11-23 15:06:21 -0500

    added search functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,27 +2,33 @@
 import React, { useState } from "react";
 import { Container,Paper, Typography,Box, Grid,Item } from '@mui/material';
 import { lightGreen } from "@mui/material/colors";
-import SearchBar from "./SearchBar";
+import {SearchBar} from "./SearchBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCaretDown} from '@fortawesome/free-solid-svg-icons';
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import DashboardTable from "./DashboardTable";
-import Watchlist from "./Watchlist";
+import Watchlist, { dataForWatchList } from "./Watchlist";
+import { useLocation } from "react-router-dom";
 
 
 // import "react-datepicker/dist/react-datepicker.css";
 
 function Dashboard() {
-
+  const [watchlistData, setWatchListData] = useState([]);
+  const userSessionData  = useLocation();
+  const setData = (data) =>{
+    setWatchListData([...data])
+  }
+  console.log(watchlistData,"JJJJJJ")
   return (
     <Container className ="main-container" maxWidth= {false} sx = {{display: "flex", flexDirection: "row",height:"100%"}}>
       <Box sx = {{display:"flex", flexDirection:"column",height:"100%", width:"35%", p:"0", mr:3}}>
 
       <Box className="search-bar" sx ={{height:"5%", mt:4, mb:1}} >
-          <SearchBar/>
+          <SearchBar  user={userSessionData} watchlistData={watchlistData} setWatchList={setData} dataForWatchList={dataForWatchList}/>
       </Box>
       <Box>
-      <Watchlist/>
+      <Watchlist user={userSessionData} dataForWatchList={dataForWatchList} watchlistData={watchlistData}/>
       </Box>
     </Box>
       <Box sx = {{display:"flex", flexDirection:"column",height:"100%", width: "65%", mt:4}}>
