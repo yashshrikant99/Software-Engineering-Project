03a20ed0ccb93e19714d7b4a9b2752afe55a47cb
siddhkasera <smk339@scarletmail.rutgers.edu>
2023-11-28 23:37:21 -0500

    i icon done

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -1,28 +1,50 @@
-import { Container,Paper, Typography,Box, Grid,TextField, RadioGroup, FormControl,FormLabel,FormControlLabel,Radio, Button, Switch,FormGroup } from '@mui/material'
-import React from 'react'
-import Popup from 'reactjs-popup';
-import Popups from './Popups';
-import BuyPopup from './BuyPopup';
-import { useState } from 'react';
-import Dashboard from './Dashboard';
-import {SearchBar} from './SearchBar';
-import Watchlist, { dataForWatchList } from './Watchlist';
+import {
+  Container,
+  Paper,
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  Button,
+  Switch,
+  FormGroup,
+} from "@mui/material";
+import React from "react";
+import Popup from "reactjs-popup";
+import Popups from "./Popups";
+import BuyPopup from "./BuyPopup";
+import { useState } from "react";
+import Dashboard from "./Dashboard";
+import { SearchBar } from "./SearchBar";
+import InfoIcon from "@mui/icons-material/Info";
+import Watchlist, { dataForWatchList } from "./Watchlist";
+import WatchlistPopup from "./WatchlistPopup";
 
 function Holdings() {
-  const [open, setOpen] = useState(false)
-  const closeModal = () => setOpen(false)
-  const userSessionData  = JSON.parse(sessionStorage.getItem("userSession"));
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  const userSessionData = JSON.parse(sessionStorage.getItem("userSession"));
   const [watchlistData, setWatchListData] = useState([]);
-  const setData = (data) =>{
-    setWatchListData([...data])
-  }
+  const setData = (data) => {
+    setWatchListData([...data]);
+  };
   const dummyStocks = [
     {
       id: 1,
       qty: 1000,
       avgPrice: 320.0,
       changePercentage: 5.09,
-      stockName: 'Stock 1',
+      stockName: "Stock 1",
       pnl: 17293.92,
       invested: 392023.02,
       ltp: 342.22,
@@ -33,109 +55,238 @@ function Holdings() {
       qty: 500,
       avgPrice: 150.0,
       changePercentage: 2.5,
-      stockName: 'Stock 2',
+      stockName: "Stock 2",
       pnl: 5000.0,
       invested: 75000.0,
       ltp: 154.5,
       ltpChange: 1.2,
     },
-  
   ];
 
   return (
- <Container maxWidth={false} sx={{ display:"flex"}}>
- <Container sx={{  display:"flex", flexDirection:"column", width:"40%", padding:"15px", alignItems:"flex-start"}}>
-  <SearchBar  user={userSessionData} watchlistData={watchlistData} setWatchList={setData} dataForWatchList={dataForWatchList}/>
-  <Watchlist user={userSessionData} dataForWatchList={dataForWatchList} watchlistData={watchlistData} /> 
-  </Container>
-<Container sx={{ height: "100vh",width:"60%", display:"flex", flexDirection:"column", alignItems:"center" }}>
-  
-   <Typography variant="h3" 
-     sx={{my:4, textAlign:'center', color:"secondary.main" }}
-     >Holdings
-     </Typography>
+    <Container maxWidth={false} sx={{ display: "flex" }}>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "40%",
+          padding: "15px",
+          alignItems: "flex-start",
+        }}
+      >
+        <SearchBar
+          user={userSessionData}
+          watchlistData={watchlistData}
+          setWatchList={setData}
+          dataForWatchList={dataForWatchList}
+        />
+        <Watchlist
+          user={userSessionData}
+          dataForWatchList={dataForWatchList}
+          watchlistData={watchlistData}
+        />
+      </Container>
+      <Container
+        sx={{
+          height: "100vh",
+          width: "60%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box className="popup-watchlist" sx={{ display: "flex" }}>
+          <Typography
+            variant="h3"
+            sx={{ my: 4, textAlign: "center", color: "secondary.main" }}
+          >
+            Holdings
+          </Typography>
 
-<Box sx={{marginBottom:7, width:"100%",}}>
-    <Paper elevation={2} sx={{bgcolor:"black", p:"2em" , display:"flex", flexDirection:"column", gap:"0.3em",  minWidth: 390, border: 0.3, width: "80%" , margin: "0 auto", color:"white"}}>
-    <Box sx={{display:"flex", justifyContent: "space-between", }}>
-      <Box>
-         <Typography variant='h5'>Invested</Typography>
-      </Box>
-      <Box>
-         <Typography variant='h5'>Current</Typography>
-      </Box>
-    </Box>
-    <Box sx={{display:"flex", justifyContent: "space-between", alignItems:"center"}}>
-         <Typography variant='h4'>14,28,927.33</Typography>
-         <Typography variant='h4'>14,82,927.33</Typography>
-    </Box>
-    <Box sx={{display:"flex", justifyContent: "space-between", alignItems:"center"}}>
-         <Typography variant='h5'>P&L</Typography>
-         <Typography sx={{color:"#03C04A"}} variant='h5'>+18,272.20</Typography>
-    </Box>
-    </Paper>
-</Box>
+          <Button onClick={handleClick}>
+            <InfoIcon fontSize="medium"> </InfoIcon>
+          </Button>
 
-{/*------- stocklist -----*/}
-<Box sx={{backgroundColor:"black" , padding: "20px", width:"90%", }}>
-   
-  <Grid container spacing={2}>
-   { dummyStocks.map((stock)=>(
-  <Grid item xs={12} key={stock.id}>
-    
-    <Paper elevation={3} sx={{p:"1em" , display:"flex", flexDirection:"column", gap:"0.3em",  minWidth: 390, marginBottom:2, padding:"20px"}} >
-    <Box sx={{display:"flex", justifyContent: "space-between", }}>
-      <Box>
-         <Typography variant='h6'>Qty.<strong>{stock.qty}</strong>&nbsp;&nbsp;Avg.<strong>{stock.avgPrice}</strong></Typography>
-      </Box>
-      <Box>
-         <Typography variant='h6' sx={{color:"#03C04A"}}> 
-         {stock.changePercentage}
-         </Typography>
-      </Box>
-    </Box>
-    <Box sx={{display:"flex", justifyContent: "space-between",}}>
-         <Typography variant='h3' sx={{color:"secondary.main",}}>{stock.stockName}</Typography>
-         <Typography variant='h6' sx={{color:"#03C04A"}}>
-          {stock.pnl}
-         </Typography>
-    </Box>
-    <Box sx={{display:"flex", justifyContent: "space-between", alignItems:"center"}}>
-         <Typography variant='h6'>Invested  <strong>{stock.invested}</strong></Typography>
-         <Typography variant='h6'>
-          LTP <strong>{stock.ltp}&nbsp;</strong> 
-        
-          <span style={{color:"red"}}>({stock.ltpChange})</span>
-          
-         </Typography>
-    </Box>
-      <Box sx={{display:"flex", flexDirection:"row", justifyContent:"center", gap:"10px", padding: "0.7em"}}>
-        <Box>
-          <Popup trigger={<Button sx={{bgcolor:"secondary.main", color:"white"}}  onClick={()=>setOpen(o=>!o)} ><Typography variant='h5' 
-          >Add</Typography></Button>} position="right center" modal nested>
-          <div>{<BuyPopup  open={open} onClose={closeModal}/>}</div>
-          </Popup>
+          {open ? (
+            <Box sx={{ mt: 4 }}>
+              <Typography sx={{ backgroundColor: "beige", p: 1 }}>
+                Refers to the list of all stocks that are bought by the user
+                along with other details like quantity, price.
+              </Typography>
+            </Box>
+          ) : null}
         </Box>
-        <Box>
-          <Popup trigger={ <Button sx={{bgcolor:"secondary.main",}} ><Typography variant='h5' sx={{color:"white", fontweight:"bold"}}>Sell</Typography></Button>} position="right center" modal>
-          <div><Popups/></div>
-          </Popup>
+
+        <Box sx={{ marginBottom: 7, width: "100%" }}>
+          <Paper
+            elevation={2}
+            sx={{
+              bgcolor: "black",
+              p: "2em",
+              display: "flex",
+              flexDirection: "column",
+              gap: "0.3em",
+              minWidth: 390,
+              border: 0.3,
+              width: "80%",
+              margin: "0 auto",
+              color: "white",
+            }}
+          >
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Box>
+                <Typography variant="h5">Invested</Typography>
+              </Box>
+              <Box>
+                <Typography variant="h5">Current</Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="h4">14,28,927.33</Typography>
+              <Typography variant="h4">14,82,927.33</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="h5">P&L</Typography>
+              <Typography sx={{ color: "#03C04A" }} variant="h5">
+                +18,272.20
+              </Typography>
+            </Box>
+          </Paper>
         </Box>
-      </Box>
-    
-  </Paper>
 
-  </Grid>))}
-  
-  </Grid>
-  
-  </Box>
-
-  </Container>
-   </Container>
-   
-  
-  )
+        {/*------- stocklist -----*/}
+        <Box
+          sx={{
+            backgroundColor: "black",
+            padding: "20px",
+            width: "90%",
+            height: "100%",
+          }}
+        >
+          <Grid container spacing={2}>
+            {dummyStocks.map((stock) => (
+              <Grid item xs={12} key={stock.id}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    p: "1em",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "0.3em",
+                    minWidth: 390,
+                    marginBottom: 2,
+                    padding: "20px",
+                  }}
+                >
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box>
+                      <Typography variant="h6">
+                        Qty.<strong>{stock.qty}</strong>&nbsp;&nbsp;Avg.
+                        <strong>{stock.avgPrice}</strong>
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" sx={{ color: "#03C04A" }}>
+                        {stock.changePercentage}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography variant="h3" sx={{ color: "secondary.main" }}>
+                      {stock.stockName}
+                    </Typography>
+                    <Typography variant="h6" sx={{ color: "#03C04A" }}>
+                      {stock.pnl}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography variant="h6">
+                      Invested <strong>{stock.invested}</strong>
+                    </Typography>
+                    <Typography variant="h6">
+                      LTP <strong>{stock.ltp}&nbsp;</strong>
+                      <span style={{ color: "red" }}>({stock.ltpChange})</span>
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      gap: "10px",
+                      padding: "0.7em",
+                    }}
+                  >
+                    <Box>
+                      <Popup
+                        trigger={
+                          <Button
+                            sx={{ bgcolor: "secondary.main", color: "white" }}
+                            onClick={() => setOpen((o) => !o)}
+                          >
+                            <Typography variant="h5">Add</Typography>
+                          </Button>
+                        }
+                        position="right center"
+                        modal
+                        nested
+                      >
+                        <div>
+                          {<BuyPopup open={open} onClose={closeModal} />}
+                        </div>
+                      </Popup>
+                    </Box>
+                    <Box>
+                      <Popup
+                        trigger={
+                          <Button sx={{ bgcolor: "secondary.main" }}>
+                            <Typography
+                              variant="h5"
+                              sx={{ color: "white", fontweight: "bold" }}
+                            >
+                              Sell
+                            </Typography>
+                          </Button>
+                        }
+                        position="right center"
+                        modal
+                      >
+                        <div>
+                          <Popups />
+                        </div>
+                      </Popup>
+                    </Box>
+                  </Box>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </Container>
+  );
 }
 
-export default Holdings
+export default Holdings;
--- src/components/WatchListButtonGroup.js
+++ src/components/WatchListButtonGroup.js
@@ -1,48 +1,54 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { Box } from '@mui/material';
-import axios from 'axios';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { Box } from "@mui/material";
+import axios from "axios";
 
-export default function WatchListBasicButtonGroup({user, watchlist, dataForWatchList, watchlistData}){
-    const delWatchlistUser = () => {
-        delWatchlist(user,watchlist)
+export default function WatchListBasicButtonGroup({
+  user,
+  watchlist,
+  dataForWatchList,
+  watchlistData,
+}) {
+  const delWatchlistUser = () => {
+    delWatchlist(user, watchlist);
+  };
+
+  async function delWatchlist(user, watchlist) {
+    try {
+      const values = {
+        short_name: watchlist["long_name"],
+      };
+      console.log(values);
+      const res = await axios
+        .delete(`http://localhost:8080/rem_watchlist/${user.id}`, {
+          data: values,
+        })
+        .then((response) => {
+          console.log(response);
+          if (response) {
+            dataForWatchList([...watchlistData]);
+          }
+        })
+        .catch((e) => {
+          console.error("Axios error", e.message);
+        });
+    } catch (e) {
+      console.error("Axios Error", e.message);
     }
-
-
-    async function delWatchlist(user,watchlist){
-        try{
-
-            const values = {
-                short_name:watchlist["long_name"]
-        }
-        console.log(values)
-            const res = await axios.delete(`http://localhost:8080/rem_watchlist/${user.id}`,{data:values})
-            .then(response=>{   
-                console.log(response)
-                if(response){
-                    dataForWatchList([...watchlistData])
-                }
-            }).catch(e=>{
-                console.error("Axios error",e.message)
-            })
-        }catch(e){
-            console.error("Axios Error",e.message)
-        }
-    }
-    return (
-        <Box
-    sx={{
-      display: 'flex',
-      justifyContent:"space-evenly",
-      alignItems: 'center',
-    }}
-  >
-
-      <Button  color='success'>Buy</Button>
-      <Button  onClick={delWatchlistUser} color='warning'>Remove</Button>
-        
-
+  }
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button color="success">Buy</Button>
+      <Button onClick={delWatchlistUser} color="warning">
+        Remove
+      </Button>
     </Box>
-    )
+  );
 }
