fa6a628f5e22ae8135703b1edbaa9b030a9ce500
Yash Shrikant <41529190+yashshrikant99@users.noreply.github.com>
2023-11-30 01:25:29 -0500

    updated code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,60 @@
-import React from "react";
-import { Container, Paper, Typography, Box, Grid, Item } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
-import "../cssfiles/NavBar.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import { Container, Paper, Typography, Box, Grid, Item } from '@mui/material'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
+import '../cssfiles/NavBar.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function NavBar() {
+function NavBar () {
+  const userSessionData = JSON.parse(sessionStorage.getItem('userSession'))
   return (
     <Container
       maxWidth={false}
       sx={{
-        display: "flex",
-        flexDirection: "row",
+        display: 'flex',
+        flexDirection: 'row',
         border: 1,
-        height: "130px",
-        width: "100%",
+        height: '130px',
+        width: '100%',
         // pt: 4.55
         p: 2,
-        justifyContent: "space-between",
-        alignItems: "center",
+        justifyContent: 'space-between',
+        alignItems: 'center'
       }}
-      className="bg-dark"
+      className='bg-dark'
     >
       {/* <Typography sx={{ color: "white", fontSize: "20px", mb: 3 }}>
         TradeMinds Playground
       </Typography> */}
       <img
-        src={require("../images/tt-logo.png")}
-        style={{ width: "5%", height: "100%" }}
+        src={require('../images/tt-logo.png')}
+        style={{ width: '5%', height: '100%' }}
       />
 
       <Box
-        className="header"
-        sx={{ display: "flex", flexDirection: "row", gap: 2 }}
+        className='header'
+        sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}
       >
-        <Link to="/dashboard" className="text-light fs-4 px-3 dashboard">
+        {/* <Link to='/dashboard' className='text-light fs-4 px-3 dashboard'>
           Dashboard
-        </Link>
-        <Link to="/holdings" className="text-light fs-4 px-3 holdings">
-          Holdings
-        </Link>
-        <Link to="/funds" className="text-light fs-4 px-3 funds">
-          Funds
-        </Link>
-        <Link to="/profile" className="text-light fs-4 px-3 profile">
-          Profile
-        </Link>
+        </Link> */}
+        {userSessionData && (
+          <Link to='/holdings' className='text-light fs-4 px-3 holdings'>
+            Holdings
+          </Link>
+        )}
+        {userSessionData && (
+          <Link to='/funds' className='text-light fs-4 px-3 funds'>
+            Funds
+          </Link>
+        )}
+        {userSessionData && (
+          <Link to='/profile' className='text-light fs-4 px-3 profile'>
+            Profile
+          </Link>
+        )}
       </Box>
       {/* <Box className="userprofile">
         <Link to="/profile" className="text-light fs-4 px-3 profile">
@@ -55,7 +62,7 @@ function NavBar() {
         </Link>
       </Box> */}
     </Container>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
